da3399dcd6c2c414591465aed71c294272304f0f
Osama <iosamaify@gmail.com>
2021-01-18 23:31:23 +0200

    change cpu command

--- blocks.h
+++ blocks.h
@@ -2,7 +2,7 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
   {"vol: ", "pamixer --get-volume-human", 0, 7},
-  {"cpu: ", "printf '%s - %s°' $(grep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {print int(usage) \"%\"}') $(sensors -j | jq --raw-output '.\"coretemp-isa-0000\".\"Core 0\" | to_entries | .[] | select(.key | match(\"input\")) | .value')", 10, 0},
+  {"cpu: ", "sensors | awk '/Core 0/ {print $3}'", 10, 0},
 	{"ram: ", "free -h | awk '/^Mem/ { print $3 }' | sed s/i//g",	10, 0},
   {"net: ", "cat /sys/class/net/e*/operstate", 10, 0},
 	{"", "date '+%a %d %b - %I:%M %P%t'", 60, 0},
