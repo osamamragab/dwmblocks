7dc3998b9a2ee8d71b39e1a38b64515330574b59
Osama <iosamaify@gmail.com>
2021-04-17 04:56:31 +0200

    add key block

--- blocks.h
+++ blocks.h
@@ -1,6 +1,7 @@
 // Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	// Icon    Command    Update Interval    Update Signal
+	{"key: ", "setxkbmap -query | awk '/layout/{print $2}'", 0, 8 },
 	{"vol: ", "pamixer --get-volume-human", 0, 7},
 	{"cpu: ", "sensors | awk '/Core 0/ {print $3}'", 10, 0},
 	{"ram: ", "free -h | awk '/^Mem/ {print substr($3, 1, length($3)-1)}'",	10, 0},
