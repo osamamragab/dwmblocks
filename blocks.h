a57bef808247aae520227566fe676f781f616b76
Osama <iosamaify@gmail.com>
2021-07-29 22:08:11 +0200

    update blocks

--- blocks.h
+++ blocks.h
@@ -5,11 +5,10 @@ static const Block blocks[] = {
 	//{"key: ", "setxkbmap -query | awk '/layout/{print $2}'", 0, 8},
 	//{"noti: ", "[ $(dunstctl is-paused) = false ] && echo on || echo off", 0, 6},
 	{"vol: ", "pamixer --get-volume-human", 0, 7},
+	{"mem: ", "free -h | awk '/^Mem/{print substr($3, 1, length($3)-1)}'", 10, 0},
 	{"cpu: ", "sensors | awk '/Core 0/{print $3}'", 10, 0},
-	{"ram: ", "free -h | awk '/^Mem/{print substr($3, 1, length($3)-1)}'", 10, 0},
 	{"net: ", "cat /sys/class/net/e*/operstate", 10, 0},
 	{"", "date '+%a %d %b - %H:%M '", 60, 0},
-	//{"", "date '+%a %d %b - %I:%M%p'", 60, 0},
 };
 
 // sets delimeter between status commands. NULL character ('\0') means no delimeter.
