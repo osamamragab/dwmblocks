8c92c4f607d51085dd4db0c5d3fb7c6dc954a490
Osama Ragab <theosamaragab@gmail.com>
2023-07-06 07:35:32 +0300

    update blocks

--- blocks.h
+++ blocks.h
@@ -4,13 +4,13 @@ static const Block blocks[] = {
 	{"", "cat /tmp/recordinginfo 2>/dev/null", 0, 9},
 	//{"key: ", "setxkbmap -query | awk '/layout/{print $2}'", 0, 8},
 	{"", "nettraf", 1, 0},
-	{"net: ", "netconstat", 5, 5},
+	{"net: ", "netconnstat", 5, 5},
 	// {"not: ", "dunstctl is-paused | sed 's/false/on/; s/true/off/'", 0, 6},
 	{"vol: ", "volumectl get", 0, 7},
-	{"mem: ", "free -h | awk '/^Mem/{print substr($3, 1, length($3)-1)}'", 5, 0},
-	{"cpu: ", "sensors | awk '/Core 0/{print $3}'", 5, 0},
-	{"bat: ", "batstat", 5, 8},
-	{"", "date '+%a %d %b - %H:%M'", 15, 0},
+	{"mem: ", "free -h | awk '/^Mem/{print substr($3, 1, length($3)-1)}'", 10, 0},
+	{"cpu: ", "sensors | awk '/Core 0/{print $3}'", 10, 0},
+	{"bat: ", "batstat", 10, 8},
+	{"", "date '+%a %d %b - %H:%M'", 60, 0},
 	{"", "echo \"#$((($(date +%s) - $(date -d 2004-06-09 +%s)) / 86400))\"", 60*60, 0},
 };
 
